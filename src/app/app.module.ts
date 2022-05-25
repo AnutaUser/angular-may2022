86a6674d178a6bb89ab7a0cbaa90e1fcbaca790a
AnutaUser <bezkorovainaanna048@gmail.com>
2022-05-25 18:22:10 +0300

    lesson5-forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from "@angular/common/http";
+import {BrowserModule} from '@angular/platform-browser';
+import {ReactiveFormsModule} from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import {RouterModule} from "@angular/router";
+import {AppComponent} from './app.component';
+import {MainLayoutComponent} from './layouts';
+import {CarsComponent} from './components/cars/cars.component';
+import {CarComponent} from './components/car/car.component';
+import {LoginComponent} from './components/login/login.component';
+import {RegisterComponent} from './components/register/register.component';
+import {HeaderComponent} from './components/header/header.component';
+import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    CarsComponent,
+    CarComponent,
+    LoginComponent,
+    RegisterComponent,
+    HeaderComponent
   ],
-    imports: [
-        BrowserModule,
-        RouterModule
-    ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
